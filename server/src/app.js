b3f9ef1816db5e6321f462f0396a6ff92b452d72
Ali Salehi <parkour.runner@gmail.com>
2023-05-30 14:46:27 +0300

    script ready

---
+++ server/src/app.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(morgan("combined"));
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.get("/*", (req, res) => {
+  res.status(200).json("I am here");
+});
+
+module.exports = app;
